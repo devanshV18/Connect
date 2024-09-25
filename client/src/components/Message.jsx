7013a612f9f5f29d9a4a3f320c7d8616b610a797
devanshV18 <devanshverma024@gmail.com>
2024-09-25 12:56:08 +0530

    completed message page ui , need to start work on the uplaod photo and video functions reusably.

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -5,6 +5,9 @@ import Avatar from './Avatar'
 import { HiOutlineDotsVertical } from "react-icons/hi"
 import { FaAngleLeft } from "react-icons/fa";
 import { MdAttachFile } from "react-icons/md";
+import { FaImages } from "react-icons/fa";
+import { RiFolderVideoFill } from "react-icons/ri";
+
 
 const Message = () => {
   const params = useParams()
@@ -19,6 +22,17 @@ const Message = () => {
     online: false,
     _id: ""
   })
+
+  const [openUpload, setOpenUpload] = useState(false)
+  
+
+  const handleUploadImage = async() => {
+    
+  }
+
+  const handleUploadVideo = () => {
+
+  }
   
   useEffect(() => {
     if(socketConnection){
@@ -70,13 +84,54 @@ const Message = () => {
               Show all Messages
        </section>       
 
-       <section className='h-16 bg-white'>
-         <div className='flex justify-center items-center w-14 h-14 rounded-sm hover:bg-violet-400'>
-              <button>
+       <section className='h-16 bg-white flex items-center'>
+         <div className='relative'>
+              <button className='flex justify-center items-center w-14 h-14 rounded-sm hover:bg-violet-400 p-4'
+                  onClick = {() => setOpenUpload(!openUpload)}
+              >
                 <MdAttachFile 
                 size={25}
                 />
               </button>
+
+              {
+                openUpload && (
+                  <div className='bg-white shadow rounded absolute bottom-16 w-36 p-2 ml-1'>
+                  <form>
+                    <label htmlFor='uplaodImage' className='flex items-center p-2 gap-3 hover:bg-slate-300 cursor-pointer'>
+                      <div className='text-violet-500'>
+                        <FaImages 
+                        size={18}
+                        />
+                      </div>
+                      <p>Images</p>
+                    </label>
+                    <label htmlFor="uploadVideo" className='flex items-center p-2 gap-3 hover:bg-slate-300 cursor-pointer'>
+                      <div className='text-violet-500'>
+                        <RiFolderVideoFill 
+                        size={18}
+                        />
+                      </div>
+                      <p>Videos</p>
+                    </label>
+
+                    <input
+                      type='file'
+                      id='uplaodImage'
+                      onChange={handleUploadImage}
+                    />
+
+                    <input
+                      type='file'
+                      id='uploadVideo'
+                      onChange={handleUploadVideo}
+                    />
+                  </form>
+                </div>
+                )
+              }
+
+              
          </div>
        </section>
 
