4f6e656f5050a83f4069d29fbb5733c9a9a00397
devanshV18 <devanshverma024@gmail.com>
2024-10-08 02:19:47 +0530

    initiated sidebar ui

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import { IoChatbubbleEllipses} from "react-icons/io5";
 import { FaUserPlus } from "react-icons/fa6";
 import { NavLink } from 'react-router-dom';
@@ -9,6 +9,8 @@ import EditUserDetails from './EditUserDetails';
 // import Divider from "./Divider"
 import { IoArrowBack } from "react-icons/io5";
 import SearchUser from './SearchUser';
+import { FaImages } from "react-icons/fa";
+import { RiFolderVideoFill } from "react-icons/ri";
 
 
 const Sidebar = () => {
@@ -16,6 +18,40 @@ const Sidebar = () => {
     const [editUserOpen, setEditUserOpen] = useState(false);
     const [allUser, setAllUser] = useState([]);
     const [openSearchUser, setOpenSearchUser] = useState(false);
+    const socketConnection = useSelector(state => state?.user?.socketConnection)
+
+    useEffect(() => {
+        if(socketConnection){
+            socketConnection.emit('sidebar', user._id)
+
+            socketConnection.on('conversation', (data) => {
+                console.log('conversation', data)
+
+                const conversationUserData = data.map((conversationUser,index) => {
+
+                    if(conversationUser?.sender?._id === conversationUser?.receiver?._id){
+                        return{
+                            ...conversationUser,
+                            userDetails : conversationUser?.sender
+                        } 
+                    }
+                    else if(conversationUser?.receiver?._id !== user?._id){
+                        return{
+                            ...conversationUser,
+                            userDetails : conversationUser?.receiver
+                        }
+                    }
+                    else{
+                        return{
+                            ...conversationUser,
+                            userDetails : conversationUser?.sender
+                        }
+                    }
+                })
+                setAllUser(conversationUserData)
+            })
+        }
+    }, [socketConnection,user])
 
   return (
     <div className='w-full h-full grid grid-cols-[48px,1fr] bg-white'>
@@ -76,6 +112,56 @@ const Sidebar = () => {
 
                         </div>
                     )
+                }
+
+                {
+                    allUser.map((conv,index) => {
+                        return(
+                            <div key={conv?._id} className='flex items-center gap-2 py-3 px-2 border border-transparent hover:border-violet-300 rounded hover:bg-slate-200 cursor-pointer'>
+                                <div>
+                                    <Avatar
+                                        imageUrl={conv?.userDetails?.profile_pic}
+                                        name={conv?.userDetails?.name}
+                                        width={40}
+                                        height={40}
+                                    />
+                                </div>
+                                <div>
+                                    <h3 className='text-ellipsis line-clamp-1 font-semibold'>{conv?.userDetails?.name}</h3>
+                                    <div className='text-slate-600 text-sm flex items-center gap-1'>
+                                        <div className='flex items-center gap-1'>
+                                            {
+                                                conv?.lastMsg?.imageUrl && (
+                                                    <div className='flex items-center gap-1'>
+                                                        <span >
+                                                         <FaImages/>
+                                                        </span>
+                                                       {!conv?.lastMsg?.text && <span>Image</span> } 
+                                                    </div>
+                                                 )
+                                            }
+
+                                            {
+                                                conv?.lastMsg?.videoUrl && (
+                                                    <div className='flex items-center gap-1'>
+                                                        <span >
+                                                         <RiFolderVideoFill/>
+                                                        </span>
+                                                       {!conv?.lastMsg?.text && <span>Video</span> } 
+                                                        
+
+                                                    </div>
+                                                    
+                                                )
+                                            }
+                                        </div>
+                                        <p className='ml-2 text-md font-bold text-slate-800'>{conv.lastMsg.text}</p>
+                                    </div>
+                                </div>
+                                <p className='text-sm w-8 h-8 flex justify-center items-center ml-auto p-1 bg-violet-200 text-black font-semibold rounded-full'>{conv?.unseenMsg}</p>
+                            </div>
+                        )
+                    })
                 }
             </div>
         </div>
--- server/socket/index.js
+++ server/socket/index.js
@@ -104,8 +104,39 @@ io.on('connection', async(socket) => {
             ]
         }).populate('messages').sort({updatedAt : -1})
         
-        io.to(data?.sender).emit('message', getConversationMessage.messages)
-        io.to(data?.receiver ).emit('message', getConversationMessage.messages)
+        io.to(data?.sender).emit('message', getConversationMessage?.messages)
+        io.to(data?.receiver ).emit('message', getConversationMessage?.messages)
+    })
+
+    //sidebar
+    socket.on('sidebar', async(currentUserId) => {
+        console.log("current user", currentUserId)
+
+        if(currentUserId){
+            const currentUserConversation = await ConversationModel.find({
+                "$or" : [
+                    { sender : currentUserId },
+                    { receiver : currentUserId } 
+                ]
+            }).sort({updatedAt : -1}).populate('messages').populate('sender').populate('receiver')
+    
+            console.log('currentUserconversation', currentUserConversation)
+    
+            const conversation = currentUserConversation.map((conv) => {
+                const countUnseenMsg = conv?.messages.reduce((preve,curr) => preve + (curr.seen ? 0 : 1),0)
+                return{
+                    _id : conv?._id,
+                    sender : conv?.sender,
+                    receiver : conv?.receiver,
+                    unseenMsg : countUnseenMsg,
+                    lastMsg: conv.messages[conv?.messages?.length - 1]
+                }
+            })
+    
+            socket.emit('conversation', conversation)
+        }
+
+        
     })
 
     socket.on('disconnect', () => {
